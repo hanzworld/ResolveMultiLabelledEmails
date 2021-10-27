da1141ebd19a081743ef65d030b86e4021e26556
hanzworld <github@barrowside.co.nz>
2021-10-27 13:13:54 +1300

    Creation of console project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FindMultiLabelledEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
